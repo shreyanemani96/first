9ca51cbe8eac0bbfd4bd95b120dc7fa4283960fa
shreyanemani96 <shreyanemani96@gmail.com>
2017-07-09 11:46:00 +0530

    Add files via upload

---
+++ 7.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("hello");
+		}
+	}
+}
